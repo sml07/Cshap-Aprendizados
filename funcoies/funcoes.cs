62aede1567ce5a1aa4e1c4f37d29663126d2e76b
Samuel <samuelbsantiago@hotmail.com>
2023-08-30 14:41:27 -0300

    .

---
+++ funcoies/funcoes.cs
@@ -0,0 +1,6 @@
+﻿void MensagemDeBoasVindas()
+{
+    Console.WriteLine("Boas vindas ao Music APP");
+}
+
+MensagemDeBoasVindas();
---
+++ projeto1/helloword.cs
@@ -0,0 +1,4 @@
+﻿// Music APP back-end
+string mensagem = "Boas vindas ao Music APP";
+
+Console.WriteLine(mensagem);
